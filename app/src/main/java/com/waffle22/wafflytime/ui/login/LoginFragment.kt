1fe03fc8f02c2949a4f1a81bbfb19045033a0701
= <=>
2022-12-27 16:37:10 +0900

    로그인 페이지 구현중...

---
+++ app/src/main/java/com/waffle22/wafflytime/ui/login/LoginFragment.kt
@@ -0,0 +1,26 @@
+package com.waffle22.wafflytime.ui.login
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.waffle22.wafflytime.databinding.FragmentLoginBinding
+
+class LoginFragment :  Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
