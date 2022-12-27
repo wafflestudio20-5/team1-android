0e2498aa13e6e9480bec6835010774c810f71099
= <=>
2022-12-27 16:21:32 +0900

    solve run error

--- app/src/main/java/com/waffle22/wafflytime/MainActivity.kt
+++ app/src/main/java/com/waffle22/wafflytime/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.waffle22.wafflytime
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("hihi","hwywhy")
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
