ae7ca21b82bad2bdf42762e63332d91f31f84b71
wjshim2003 <wjshim@snu.ac.kr>
2023-01-05 23:45:28 +0900

    게시판 검색 화면 레이아웃 및 fragment 추가

---
+++ app/src/main/java/com/waffle22/wafflytime/ui/boards/BoardSearchFragment.kt
@@ -0,0 +1,42 @@
+package com.waffle22.wafflytime.ui.boards
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.waffle22.wafflytime.databinding.FragmentSearchBoardBinding
+
+class BoardSearchFragment : Fragment() {
+    private lateinit var binding: FragmentSearchBoardBinding
+
+    private val viewModel: BoardSearchViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSearchBoardBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (viewModel.searchResults.value!!.isEmpty())
+            binding.searchResult.visibility = View.GONE
+        else    binding.noSearchResult.visibility = View.GONE
+
+        val boardSearchAdapter  = BoardSearchAdapter()
+        viewModel.searchResults.observe(this.viewLifecycleOwner) { items ->
+            items.let{
+                boardSearchAdapter.submitList(it)
+            }
+        }
+        binding.searchResult.adapter = boardSearchAdapter
+        binding.searchResult.layoutManager = LinearLayoutManager(this.context)
+    }
+}
