f51400c2b93f12b97a0a8deeb8c37d504a45f83a
= <=>
2023-01-05 21:59:47 +0900

    알림 기본구현 완료

--- app/src/main/java/com/waffle22/wafflytime/ui/notification/ChatBoxFragment.kt
+++ app/src/main/java/com/waffle22/wafflytime/ui/notification/ChatBoxFragment.kt
@@ -1,4 +1,40 @@
 package com.waffle22.wafflytime.ui.notification
 
-class ChatBoxFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.waffle22.wafflytime.databinding.FragmentChatboxBinding
+import com.waffle22.wafflytime.databinding.FragmentNotificationBinding
+import com.waffle22.wafflytime.network.dto.Chat
+import com.waffle22.wafflytime.network.dto.Notification
+import com.waffle22.wafflytime.util.ChatAdapter
+import com.waffle22.wafflytime.util.NotifyAdapter
+
+class ChatBoxFragment : Fragment() {
+    private lateinit var binding : FragmentChatboxBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentChatboxBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = ChatAdapter()
+        var hello: List<Chat> = listOf(Chat("diluc","sword","0101"), Chat("pai","cute","12"))
+        adapter.submitList(hello)
+
+        binding.apply {
+            chatBoxRecyclerView.adapter = adapter
+        }
+
+
+    }
 }
---
+++ app/src/main/java/com/waffle22/wafflytime/ui/notification/NotificationPageAdapter.kt
@@ -0,0 +1,4 @@
+package com.waffle22.wafflytime.ui.notification
+
+class NotificationPageAdapter {
+}
