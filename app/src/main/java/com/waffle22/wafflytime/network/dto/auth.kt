03fe31343c09d363bcba7b2b63b250bb7c12fea6
= <=>
2023-01-07 01:47:59 +0900

    Login, SignUp Develop

--- app/src/main/java/com/waffle22/wafflytime/network/dto/auth.kt
+++ app/src/main/java/com/waffle22/wafflytime/network/dto/auth.kt
@@ -8,15 +8,16 @@ import com.squareup.moshi.Json
  */
 
 data class LoginRequest(
-    @Json(name = "username") val userId: String,
+    @Json(name = "id") val userId: String,
     @Json(name = "password") val userPassword: String
 )
 
 data class SignUpRequest(
-    @Json(name = "username") val userId: String,
+    @Json(name = "id") val userId: String,
     @Json(name = "password") val userPassword: String
 )
 
-data class AccessTokenContainer(
-    @Json(name = "accessToken") val accessToken: String
+data class TokenContainer(
+    @Json(name = "accessToken") val accessToken: String,
+    @Json(name = "refreshToken") val refreshToken: String
 )
--- app/src/main/java/com/waffle22/wafflytime/ui/login/SignUpFragment.kt
+++ app/src/main/java/com/waffle22/wafflytime/ui/login/SignUpFragment.kt
@@ -7,11 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.waffle22.wafflytime.R
 import com.waffle22.wafflytime.databinding.FragmentSignupBinding
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class SignUpFragment : Fragment() {
     private lateinit var binding: FragmentSignupBinding
-    private val viewModel: LoginViewModel by activityViewModels()
+    private val viewModel: SignUpViewModel by sharedViewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,9 +32,34 @@ class SignUpFragment : Fragment() {
             btnSignup.setOnClickListener { signUp() }
         }
 
+        lifecycleScope.launchWhenStarted {
+            viewModel.signUpState.collect {
+                when (it){
+                    "StandBy" -> {
+                        null
+                    }
+                    "SignUpOk" -> {
+                        // TODO: 회원가입 성공했다는 메세지를 토스트메세지로 띄워줄것
+                        // TODO: 뒤로가기 누르면 로그인페이지로 다시 못오게 해야됨
+                        findNavController().navigate(R.id.action_signUpFragment_to_baseNotificationFragment)
+                    }
+                    "SignUpConflict" -> {
+                        Log.d("debug"," signup Failed by conflict")
+                    } // TODO: Generate ToastMessage (Login Failed, Check Id or Password)
+                    else -> {
+                        Log.d("debug","unexpected error of signup")
+                    } // TODO: Generate ToastMessage (Unknown Error Occurred)
+                }
+                if (it != "StandBy"){
+                    viewModel.resetSignUpState()
+                }
+            }
+        }
+
     }
 
     private fun signUp(){
+        // TODO: 이거 눌렸을때 다른버튼 안눌리게 하기, 한번 더누르거나 그런거 방지
         viewModel.signUp(binding.idEditText.text.toString(), binding.passwordEditText.text.toString())
     }
 }
