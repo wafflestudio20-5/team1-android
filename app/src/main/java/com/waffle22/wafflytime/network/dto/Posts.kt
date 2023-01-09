85a0bff6c290ea7582a033d45f29fb6e7cc8bd9a
wjshim2003 <wjshim@snu.ac.kr>
2023-01-09 22:24:02 +0900

    Board, Post 관련 API suspend 함수 구현 (pagenation 제외)

--- app/src/main/java/com/waffle22/wafflytime/network/dto/Posts.kt
+++ app/src/main/java/com/waffle22/wafflytime/network/dto/Posts.kt
@@ -2,7 +2,7 @@ package com.waffle22.wafflytime.network.dto
 
 import com.squareup.moshi.Json
 
-data class BoardDTO(
+data class BoardDTO(    //BoardResponse
     @Json(name = "boardId") val boardId: Long,
     @Json(name = "boardType") val boardType: String,
     @Json(name = "title") val title: String,
@@ -10,15 +10,70 @@ data class BoardDTO(
     @Json(name = "allowAnonymous") val allowAnonymous: Boolean
 )
 
-data class PostDTO( // PostResponse
+data class ImageRequest(
+    @Json(name = "imageId") val imageId: Int,
+    @Json(name = "fileName") val fileName: String,
+    @Json(name = "description") val description: String
+)
+
+data class PostRequest(
+    @Json(name = "title") val title: String,
+    @Json(name = "contents") val contents: String,
+    @Json(name = "isQuestion") val isQuestion: Boolean,
+    @Json(name = "isWriterAnonymous") val isWriterAnonymous : Boolean,
+    @Json(name = "images") val images: List<ImageRequest>
+)
+
+data class ImageResponse(
+    @Json(name = "imageId") val imageId: Int,
+    @Json(name = "preSignedUrl") val preSignedUrl: String,
+    @Json(name = "description") val description: String
+)
+
+data class EditPostRequest(
+    @Json(name = "title") val title: String,
+    @Json(name = "contents") val contents: String,
+    @Json(name = "isQuestion") val isQuestion: Boolean,
+    @Json(name = "isWriterAnonymous") val isWriterAnonymous : Boolean,
+    @Json(name = "images") val images: List<ImageRequest>,
+    @Json(name = "deletedImages") val deletedImages: List<String>
+)
+
+data class PostResponse( // PostResponse
+    @Json(name = "postId") val postId: Long,
+    @Json(name = "writerId") val writerId: Long,
+    @Json(name = "isWriterAnonymous") val isWriterAnonymous : Boolean,
+    @Json(name = "title") val title: String,
+    @Json(name = "contents") val contents: String,
+    @Json(name = "images") val images: List<ImageResponse>
+)
+
+data class CreateBoardResponse(
+    @Json(name = "userId") val userId : Long,
+    @Json(name = "boardId") val boardId: Long,
+    @Json(name = "boardType") val boardType: String,
+    @Json(name = "title") val title: String,
+    @Json(name = "description") val description: String,
+    @Json(name = "allowAnonymous") val allowAnonymous: Boolean
+)
+
+data class DeleteBoardResponse(
+    @Json(name = "boardId") val boardId: Long,
+    @Json(name = "title") val title: String
+)
+
+data class DeletePostResponse(
+    @Json(name = "boardId") val boardId: Long,
+    @Json(name = "boardTitle") val boardTitle: String,
+    @Json(name = "postId") val postId: Long,
+    @Json(name = "postTitle") val postTitle: String
+)
+
+data class EditPostResponse(
     @Json(name = "postId") val postId: Long,
     @Json(name = "writerId") val writerId: Long,
     @Json(name = "isWriterAnonymous") val isWriterAnonymous : Boolean,
     @Json(name = "title") val title: String,
     @Json(name = "contents") val contents: Boolean
-)
-
-data class PostResponse(
-    val result: PostDTO
 )
 
