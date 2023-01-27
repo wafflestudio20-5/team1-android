5ff0206ba1ed2d4f5267b01cff5bb01f194415da
seozzi <seojihee1004@snu.ac.kr>
2023-01-27 23:21:35 +0900

    transfer

--- app/src/main/java/com/waffle22/wafflytime/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/waffle22/wafflytime/ui/login/LoginViewModel.kt
@@ -1,5 +1,6 @@
 package com.waffle22.wafflytime.ui.login
 
+import android.content.ContentValues.TAG
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.squareup.moshi.Moshi
@@ -11,6 +12,11 @@ import com.waffle22.wafflytime.util.parseError
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import android.content.Context
+import android.util.Log
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
 
@@ -21,6 +27,7 @@ class LoginViewModel(
     private val wafflyApiService: WafflyApiService,
     private val authStorage: AuthStorage,
     private val moshi: Moshi
+
 ) : ViewModel() {
 
     // TODO: Change String type to Enum Class!!!
@@ -59,9 +66,35 @@ class LoginViewModel(
 
 
     fun kakaoSocialLogin(context: Context) {
-        val client_id = "14e86042a3842d295c4ef5af422fac3d"
-        val redirect_uri = "http://localhost:3000"
-        
+        val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            if (error != null) {
+                Log.e(TAG, "카카오계정으로 로그인 실패", error)
+            } else if (token != null) {
+                Log.i(TAG, "카카오계정으로 로그인 성공 ${token.accessToken}")
+            }
+        }
+
+// 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
+            UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
+                if (error != null) {
+                    Log.e(TAG, "카카오톡으로 로그인 실패", error)
+
+                    // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                    // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                    if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                        return@loginWithKakaoTalk
+                    }
+
+                    // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                    UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+                } else if (token != null) {
+                    Log.i(TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
+                }
+            }
+        } else {
+            UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+        }
 
     }
 
---
+++ app/src/main/java/com/waffle22/wafflytime/ui/login/SignUpSocialFragment.kt
@@ -0,0 +1,47 @@
+package com.waffle22.wafflytime.ui.login
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.waffle22.wafflytime.R
+import com.waffle22.wafflytime.databinding.FragmentSignupBinding
+import com.waffle22.wafflytime.databinding.FragmentSignupSocialBinding
+import com.waffle22.wafflytime.util.StateStorage
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+
+class SignUpSocialFragment : Fragment() {
+    private lateinit var binding: FragmentSignupSocialBinding
+    private val viewModel: SignUpViewModel by sharedViewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSignupSocialBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            btnSignUpDone.setOnClickListener { }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.signUpState.collect {
+                //signUpLogic(it)
+            }
+        }
+    }
+}
