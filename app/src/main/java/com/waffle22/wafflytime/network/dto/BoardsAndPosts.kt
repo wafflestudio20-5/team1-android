8d9c10760db7dfc839278f397e6e16476f75f640
arkrangian <darkskylord24@gmail.com>
2023-01-31 18:34:19 +0900

    PostViewModel에서 canEditPost부분 구현

--- app/src/main/java/com/waffle22/wafflytime/network/dto/BoardsAndPosts.kt
+++ app/src/main/java/com/waffle22/wafflytime/network/dto/BoardsAndPosts.kt
@@ -63,6 +63,7 @@ data class PostResponse( // PostResponse
     @Json(name = "writerId") val writerId: Long,
     @Json(name = "nickname") val nickname: String?,
     @Json(name = "isWriterAnonymous") val isWriterAnonymous : Boolean,
+    @Json(name = "isMyPost") val isMyPost: Boolean,
     @Json(name = "isQuestion") val isQuestion: Boolean,
     @Json(name = "title") val title: String?,
     @Json(name = "contents") val contents: String,
