d91533dfed214fe4b9ae65d2a4203c975c118a64
wjshim2003 <wjshim@snu.ac.kr>
2023-02-03 13:59:15 +0900

    이미지에 설명 첨부 가능

--- app/src/main/java/com/waffle22/wafflytime/ui/boards/post/newpost/NewPostImageAdapter.kt
+++ app/src/main/java/com/waffle22/wafflytime/ui/boards/post/newpost/NewPostImageAdapter.kt
@@ -1,8 +1,12 @@
 package com.waffle22.wafflytime.ui.boards.post.newpost
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.content.Context
+import android.content.DialogInterface
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -40,8 +44,18 @@ class NewPostImageAdapter(
                 .load(image.byteArray)
                 .into(binding.image)
             binding.addDescription.setOnClickListener {
-                var newDescription = ""
-                onEditDescription(image.imageRequest, newDescription)
+                val editText = EditText(context)
+                editText.setText(image.imageRequest.description)
+                val alertBuilder = AlertDialog.Builder(context)
+                alertBuilder.setTitle("이미지 설명을 입력하세요")
+                alertBuilder.setView(editText)
+                alertBuilder.setPositiveButton("확인"
+                ) { _, _ ->
+                    onEditDescription(image.imageRequest, editText.text.toString())
+                }
+                alertBuilder.setNegativeButton("취소"
+                ) { _, _ -> null }
+                alertBuilder.show()
             }
             binding.deleteImage.setOnClickListener {
                 onDeleteImage(image.imageRequest)
